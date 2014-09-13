98d90b4fddffbcd4d772381d43887b5b304ba7ad
Yasser Developer <yasser.dwyan@gmail.com>
2014-09-13 17:36:53 +1000

    Added the following: Creating tables codes in dbhelper, inserting a photo into db, retreiving a photo from db(need to deal with a cursor) and additional setter/getter methods to Photo class.

--- PhotoApp/src/com/example/photo/Photo.java
+++ PhotoApp/src/com/example/photo/Photo.java
@@ -13,31 +13,45 @@ import android.graphics.Bitmap;
 public class Photo 
 {	
 
+	private final String YES = "yes";
+	private final String NO = "no";
+	private final String NONE = "none";
+	
 	private String name;
 	//A photo ID is the same as the timestamp.
 	//A date taken for the photo in String, and the value of this attribute has to be in a unified 
 	//format e.g yyyyMMdd_HHmmss. For consistency, the value has to be from the photo manager.
 	private String photoID;
 	
+	//A photo decription
+	private String description;
+	
 	//An image data for a photo
 	private Bitmap bitmap;
 	//Album name where a photo belongs to, if applicable. Otherwise, null
-	private String album;
+	private String album = NONE;
 	
+	private boolean isUploadedToServer = false;
 	
-	public Photo(String name, String photoID, Bitmap bitmap) {
+	public Photo() {
+	}	
+	
+	public Photo(String name, String photoID, Bitmap bitmap, boolean uploadedToServer) {
 		//super();
 		this.name = name;
 		this.photoID = photoID;
 		this.bitmap = bitmap;
+		this.isUploadedToServer = uploadedToServer;
 	}
 
-	public Photo(String name, String photoID, Bitmap bitmap, String album) {
+	public Photo(String name, String photoID, Bitmap bitmap, String album,
+			boolean uploadedToServer) {
 		//super();
 		this.name = name;
 		this.photoID = photoID;
 		this.bitmap = bitmap;
 		this.album = album;
+		this.isUploadedToServer = uploadedToServer;
 	}
 
 	public String getName() {
@@ -67,6 +81,39 @@ public class Photo
 
 	public void setAlbum(String album) {
 		this.album = album;
+	}
+
+	public boolean isUploadedToServer() {
+		return isUploadedToServer;
+	}
+	public String isUploadedToServerAsYesNO()
+	{
+		if(isUploadedToServer)
+			return YES;
+		
+		return NO;
+	}
+	public void setUploadedToServer(boolean isUploadedToServer) {
+		this.isUploadedToServer = isUploadedToServer;
+	}
+	
+	public void setUploadedToServer(String isUploadedToServerStr) {
+		if(isUploadedToServerStr.equalsIgnoreCase(YES))
+		{
+			this.isUploadedToServer = true;
+		}
+		else
+		{
+			this.isUploadedToServer = false;
+		}
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 }
