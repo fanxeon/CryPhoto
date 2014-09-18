2d89c2dd0349f72f752475c1692d10d6ecbf218b
Yasser Developer <yasser.dwyan@gmail.com>
2014-09-19 00:23:30 +1000

    Search string added.

--- PhotoApp/gen/com/example/photoapp/R.java
+++ PhotoApp/gen/com/example/photoapp/R.java
@@ -8,6 +8,9 @@
 package com.example.photoapp;
 
 public final class R {
+    public static final class array {
+        public static final int ImgRef=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,30 +25,35 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_action_settings=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int btnDB=0x7f080003;
-        public static final int btnTakePhoto=0x7f080001;
-        public static final int imageViewPicTaken=0x7f080002;
-        public static final int imgViewDB=0x7f080004;
-        public static final int textView1=0x7f080000;
-        public static final int txtViewDesc=0x7f080005;
+        public static final int action_search=0x7f090007;
+        public static final int action_settings=0x7f090006;
+        public static final int btnDB=0x7f090003;
+        public static final int btnTakePhoto=0x7f090001;
+        public static final int imageViewPicTaken=0x7f090002;
+        public static final int imgViewDB=0x7f090004;
+        public static final int textView1=0x7f090000;
+        public static final int txtViewDesc=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_tester=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int tester=0x7f070001;
+        public static final int main=0x7f080000;
+        public static final int main_activity_actions=0x7f080001;
+        public static final int tester=0x7f080002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int title_activity_tester=0x7f050003;
+        public static final int action_search=0x7f060004;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int title_activity_tester=0x7f060003;
     }
     public static final class style {
         /** 
@@ -69,10 +77,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
