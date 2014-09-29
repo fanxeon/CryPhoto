696c3909d4b6c1220e7aeb6e266d4d3f535695ed
Yasser Developer <yasser.dwyan@gmail.com>
2014-09-29 21:47:24 +1000

    to make github happy to switch to another branch!!!!

--- PhotoApp/gen/com/example/photoapp/R.java
+++ PhotoApp/gen/com/example/photoapp/R.java
@@ -8,6 +8,9 @@
 package com.example.photoapp;
 
 public final class R {
+    public static final class array {
+        public static final int ImgRef=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -23,37 +26,42 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_action_download=0x7f020000;
-        public static final int ic_action_upload=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_action_settings=0x7f020002;
+        public static final int ic_action_upload=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int btnDB=0x7f080003;
-        public static final int btnDownload=0x7f080008;
-        public static final int btnServerRemove=0x7f080009;
-        public static final int btnSync=0x7f08000b;
-        public static final int btnTakePhoto=0x7f080005;
-        public static final int btnUploadPhoto=0x7f080006;
-        public static final int imageViewPicTaken=0x7f080002;
-        public static final int imgViewDB=0x7f080001;
-        public static final int imgViewDownload=0x7f080007;
-        public static final int txtVStatuRemove=0x7f08000a;
-        public static final int txtViewDesc=0x7f080004;
-        public static final int txtViewUploadStatus=0x7f080000;
+        public static final int action_search=0x7f09000d;
+        public static final int action_settings=0x7f09000c;
+        public static final int btnDB=0x7f090003;
+        public static final int btnDownload=0x7f090008;
+        public static final int btnServerRemove=0x7f090009;
+        public static final int btnSync=0x7f09000b;
+        public static final int btnTakePhoto=0x7f090005;
+        public static final int btnUploadPhoto=0x7f090006;
+        public static final int imageViewPicTaken=0x7f090002;
+        public static final int imgViewDB=0x7f090001;
+        public static final int imgViewDownload=0x7f090007;
+        public static final int txtVStatuRemove=0x7f09000a;
+        public static final int txtViewDesc=0x7f090004;
+        public static final int txtViewUploadStatus=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_tester=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int tester=0x7f070001;
+        public static final int main=0x7f080000;
+        public static final int main_activity_actions=0x7f080001;
+        public static final int tester=0x7f080002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int title_activity_tester=0x7f050003;
+        public static final int action_search=0x7f060004;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int title_activity_tester=0x7f060003;
     }
     public static final class style {
         /** 
@@ -77,10 +85,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
